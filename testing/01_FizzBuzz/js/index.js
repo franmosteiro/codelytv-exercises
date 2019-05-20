891f7d5341471ac81a647d56124adb38953465ec
Fran Mosteiro <fran.mosteiro@gmail.com>
2019-05-20 21:29:26 +0200

    change mod subject

--- testing/01_FizzBuzz/js/index.js
+++ testing/01_FizzBuzz/js/index.js
@@ -1,12 +1,21 @@
 function fizzbuzz(number) {
-    if (number % 3 === 0 && number % 5 === 0) {
+
+    if (number % 15 === 0) {
+    
         return 'FizzBuzz';
+    
     } else if (number % 3 === 0) {
+    
         return 'Fizz';
+    
     } else if (number % 5 === 0) {
+    
         return 'Buzz';
+    
     } else {
+    
         return number;
+    
     }
 }
 
